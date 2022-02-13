7e547158428cebac9fcafc5ed491d238c5e24fe6
Humberto García <humbertodev14@gmail.com>
2022-02-12 20:37:15 -0400

    DB.json agregada en comentario en el componente App

--- src/Components/App.js
+++ src/Components/App.js
@@ -16,8 +16,194 @@ import Header from "./Organims/Header";
 import './App.css'; 
 import "../styles/styles.scss";
 const App = () => {
+  /* {
+    "especialidades": [
+      {
+        "id": 1,
+        "name": "json-server"
+      },
+      {
+        "id": 2,
+        "name": "Typescript"
+      },
+      {
+        "id": 3,
+        "name": "Typescript"
+      },
+      {
+        "id": 4,
+        "name": "Typescript"
+      },
+      {
+        "id": 5,
+        "name": "Typescript"
+      },
+      {
+        "id": 6,
+        "name": "Typescript"
+      }
+    ],
+    "profesores": [
+      {
+        "id": 1,
+        "name": "Humberto García",
+        "country": "Venezuela",
+        "picture": "https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg"
+      },
+      {
+        "id": 2,
+        "name": "Daniel Moya",
+        "country": "Venezuela",
+        "picture": "https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg"
+      },
+      {
+        "id": 3,
+        "name": "Daniel Moya",
+        "country": "Venezuela",
+        "picture": "https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg"
+      },
+      {
+        "id": 4,
+        "name": "Daniel Moya",
+        "country": "Venezuela",
+        "picture": "https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg"
+      },
+      {
+        "id": 5,
+        "name": "Daniel Moya",
+        "country": "Venezuela",
+        "picture": "https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg"
+      },
+      {
+        "id": 6,
+        "name": "Daniel Moya",
+        "country": "Venezuela",
+        "picture": "https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg"
+      },
+      {
+        "id": 7,
+        "name": "Daniel Moya",
+        "country": "Venezuela",
+        "picture": "https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg"
+      },
+      {
+        "id": 8,
+        "name": "Daniel Moya",
+        "country": "Venezuela",
+        "picture": "https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg"
+      },
+      {
+        "id": 9,
+        "name": "Daniel Moya",
+        "country": "Venezuela",
+        "picture": "https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg"
+      },
+      {
+        "id": 10,
+        "name": "Daniel Moya",
+        "country": "Venezuela",
+        "picture": "https://cdn.pixabay.com/photo/2016/03/27/17/42/man-1283235_960_720.jpg"
+      }
+    ],
+    "cursos": [
+      {
+        "id": 1,
+        "name": "typicode"
+      },
+      {
+        "id": 2,
+        "name": "typescript"
+      },
+      {
+        "id": 3,
+        "name": "Angular"
+      },
+      {
+        "id": 4,
+        "name": "Angular"
+      },
+      {
+        "id": 5,
+        "name": "Angular"
+      },
+      {
+        "id": 6,
+        "name": "Angular"
+      },
+      {
+        "id": 7,
+        "name": "Angular"
+      },
+      {
+        "id": 8,
+        "name": "Angular"
+      },
+      {
+        "id": 9,
+        "name": "Angular"
+      },
+      {
+        "id": 10,
+        "name": "Angular"
+      },
+      {
+        "id": 11,
+        "name": "Angular"
+      }
+    ],
+    "login": [
+      {
+        "email": "humbertodev14@gmail.com",
+        "password": "21312",
+        "id": 1
+      },
+      {
+        "email": "humbertodev14@gmail.com",
+        "password": "213",
+        "id": 2
+      },
+      {
+        "email": "humbertodev14@gmail.com",
+        "password": "132",
+        "id": 3
+      }
+    ],
+    "signup": [],
+    "posts": [
+      {
+        "id": 1,
+        "author": "Jose Millan",
+        "title": "Ayuda con CSS grid Urgente!!",
+        "fecha": "20/3/2021 a las 10:45 AM",
+        "content": "daojdojsadfhdsoahf suahfushdfuhsuaifhusdhaf fshfusdhfushufh suhdfshfhsodiahfopisdha oshfosahfihsfhsuhf opshfopishfohsifhiosdh oshfioshafiohsdfhsoiahfiop sahfdioshfoishfoihsiofh oiashfoipsafhposhafopihs osdhfioshafiophsafoisa sopafh oasihf asoifhp aspofhapsofdih poaihsfdpohaspfh poashf poashf poashf"
+      },
+      {
+        "id": 2,
+        "author": "Jose Vergara",
+        "title": "Ayuda con CSS grid Urgente!!",
+        "fecha": "10/3/2021 a las 9:45 PM",
+        "content": "daojdojsadfhdsoahf suahfushdfuhsuaifhusdhaf fshfusdhfushufh suhdfshfhsodiahfopisdha oshfosahfihsfhsuhf opshfopishfohsifhiosdh oshfioshafiohsdfhsoiahfiop sahfdioshfoishfoihsiofh oiashfoipsafhposhafopihs osdhfioshafiophsafoisa sopafh oasihf asoifhp aspofhapsofdih poaihsfdpohaspfh poashf poashf poashf"
+      },
+      {
+        "id": 3,
+        "author": "Gabriela Millan",
+        "title": "Ayuda con JAVA Urgente!!",
+        "fecha": "20/3/2020 a las 10:45 PM",
+        "content": "daojdojsadfhdsoahf suahfushdfuhsuaifhusdhaf fshfusdhfushufh suhdfshfhsodiahfopisdha oshfosahfihsfhsuhf opshfopishfohsifhiosdh oshfioshafiohsdfhsoiahfiop sahfdioshfoishfoihsiofh oiashfoipsafhposhafopihs osdhfioshafiophsafoisa sopafh oasihf asoifhp aspofhapsofdih poaihsfdpohaspfh poashf poashf poashf"
+      },
+      {
+        "id": 4,
+        "author": "Lenny Perez",
+        "title": "Ayuda con HTML Urgente!!",
+        "fecha": "10/2/2021 a las 11:25 AM",
+        "content": "daojdojsadfhdsoahf suahfushdfuhsuaifhusdhaf fshfusdhfushufh suhdfshfhsodiahfopisdha oshfosahfihsfhsuhf opshfopishfohsifhiosdh oshfioshafiohsdfhsoiahfiop sahfdioshfoishfoihsiofh oiashfoipsafhposhafopihs osdhfioshafiophsafoisa sopafh oasihf asoifhp aspofhapsofdih poaihsfdpohaspfh poashf poashf poashf"
+      }
+    ]
+  } */
+  
   return (
     <div>
+      
       <Header />
       <Routes>
         <Route
