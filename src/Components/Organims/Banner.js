3584c820252d85c908ec628d50df290a3c53df08
Humberto García <humbertodev14@gmail.com>
2022-02-19 18:26:34 -0400

    db.json added

--- src/Components/Organims/Banner.js
+++ src/Components/Organims/Banner.js
@@ -5,17 +5,17 @@ const Banner = ({ image, title, subtitle, home, colorFrom, colorTo }) => (
   <div>
     {home ? (
       <section
-        className={`text-gray-600 body-font bg-gradient-to-t ${colorFrom} ${colorTo} `}
+        classNameName={`text-gray-600 body-font bg-gradient-to-t ${colorFrom} ${colorTo} `}
       >
-        <div className="container h-80 mx-auto flex flex-wrap items-center">
-          <h2 className="sm:text-3xl text-2xl text-white font-medium title-font mb-2 md:w-2/5">
+        <div classNameName="container h-80 mx-auto flex flex-wrap items-center">
+          <h2 classNameName="sm:text-3xl text-2xl text-white font-medium title-font mb-2 md:w-2/5">
             {title}
           </h2>
-          <div className="md:w-3/5 md:pl-6">
-            <p className="leading-relaxed text-gray-400">{subtitle}</p>
-            <div className="flex md:mt-4 mt-6">
+          <div classNameName="md:w-3/5 md:pl-6">
+            <p classNameName="leading-relaxed text-gray-400">{subtitle}</p>
+            <div classNameName="flex md:mt-4 mt-6">
               <NavLink to="/cursos">
-                <button className="inline-flex text-white bg-cyan-700 border-0 py-1 px-4 focus:outline-none hover:bg-cyan-800 rounded">
+                <button classNameName="inline-flex text-white bg-cyan-700 border-0 py-1 px-4 focus:outline-none hover:bg-cyan-800 rounded">
                   Ver cursos
                 </button>
               </NavLink>
@@ -24,19 +24,19 @@ const Banner = ({ image, title, subtitle, home, colorFrom, colorTo }) => (
         </div>
       </section>
     ) : (
-      <section class={`text-gray-400 bg-gradient-to-t ${colorFrom} ${colorTo} body-font`}>
-        <div class="container  py-10 mx-auto flex flex-wrap">
-          <div class="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
-            <div class="w-full sm:p-4 px-4 mb-6">
-              <h1 class="title-font font-medium text-xl mb-2 text-white">
+      <section className={`text-gray-400 bg-gradient-to-t ${colorFrom} ${colorTo} body-font`}>
+        <div className="container  py-10 mx-auto flex flex-wrap">
+          <div className="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
+            <div className="w-full sm:p-4 px-4 mb-6">
+              <h1 className="title-font font-medium text-xl mb-2 text-white">
                 {title}
               </h1>
-              <div class="leading-relaxed">{subtitle}</div>
+              <div className="leading-relaxed">{subtitle}</div>
             </div>
           </div>
-          <div class="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
+          <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
             <img
-              class="object-cover object-center w-full max-h-80"
+              className="object-cover object-center w-full max-h-80"
               src={image.src}
               alt={image.alt}
             />
