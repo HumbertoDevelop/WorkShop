0f47883170bc94d6968b3a978ab52e5abb1538ed
HumbertoDevelop <humbertodev14@gmail.com>
2022-03-23 21:35:09 -0400

    Fragment.js removed from App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,6 @@
-import React, { useCallback, useEffect } from 'react';
+/** @format */
+
+import React, { useCallback, useEffect } from "react";
 import Home from "./Pages/Home";
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./Routes/Protected";
@@ -7,7 +9,6 @@ import Speciality from "./Pages/Speciality";
 import Course from "./Pages/Course";
 import Courses from "./Pages/Courses";
 import Teachers from "./Pages/Teachers";
-import Fragment from "./Pages/Fragment";
 import PublicRoute from "./Routes/Public";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
@@ -20,111 +21,100 @@ import { useDispatch } from "react-redux";
 import { bulkCart } from "../Redux/actionCreators";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const updateCart = useCallback(
-      () => {
-          dispatch(bulkCart());
-      },
-      [dispatch],
-  );
+	const dispatch = useDispatch();
+	const updateCart = useCallback(() => {
+		dispatch(bulkCart());
+	}, [dispatch]);
 
-  useEffect(() => {
-    updateCart();
-  }, [updateCart]);
+	useEffect(() => {
+		updateCart();
+	}, [updateCart]);
 
-  return (
-    <div  >
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/especialidades"
-          element={
-            <ProtectedRoute>
-              <Specialitities />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/especialidades/:id"
-          element={
-            <ProtectedRoute>
-              <Speciality />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/cursos"
-          element={
-            <ProtectedRoute>
-              <Courses />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/cursos/:id"
-          element={
-            <ProtectedRoute>
-              <Course />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/profesores"
-          element={
-            <ProtectedRoute>
-              <Teachers />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/carrito"
-          element={
-            <ProtectedRoute>
-              <Cart />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/clase/:id"
-          element={
-            <ProtectedRoute>
-              <Fragment />
-            </ProtectedRoute>
-          }
-        />
+	return (
+		<div>
+			<Header />
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<ProtectedRoute>
+							<Home />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/especialidades"
+					element={
+						<ProtectedRoute>
+							<Specialitities />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/especialidades/:id"
+					element={
+						<ProtectedRoute>
+							<Speciality />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/cursos"
+					element={
+						<ProtectedRoute>
+							<Courses />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/cursos/:id"
+					element={
+						<ProtectedRoute>
+							<Course />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/profesores"
+					element={
+						<ProtectedRoute>
+							<Teachers />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path="/carrito"
+					element={
+						<ProtectedRoute>
+							<Cart />
+						</ProtectedRoute>
+					}
+				/>
 
-        {/* Public Routes */}
-        <Route
-          path="/login"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <PublicRoute>
-              <Register />
-            </PublicRoute>
-          }
-        />
+				{/* Public Routes */}
+				<Route
+					path="/login"
+					element={
+						<PublicRoute>
+							<Login />
+						</PublicRoute>
+					}
+				/>
+				<Route
+					path="/register"
+					element={
+						<PublicRoute>
+							<Register />
+						</PublicRoute>
+					}
+				/>
 
-        {/* Default Route */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
+				{/* Default Route */}
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
